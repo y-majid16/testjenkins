d89e7d8faf44c48fd6acc313456b90ff14418f1f
y-majid16 <yaumulmajid@gmail.com>
2023-06-15 16:23:54 +0700

    first commit

---
+++ src/test/java/com/example/latihan/LatihanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latihan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
