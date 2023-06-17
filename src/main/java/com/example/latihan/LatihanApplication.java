7a8d230f2ebf33b2696d04c2a975a2aa393960fe
y-majid16 <yaumulmajid@gmail.com>
2023-06-17 20:54:09 +0700

    deploy

--- src/main/java/com/example/latihan/LatihanApplication.java
+++ src/main/java/com/example/latihan/LatihanApplication.java
@@ -3,9 +3,10 @@ package com.example.latihan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LatihanApplication implements CommandLineRunner {
+public class LatihanApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LatihanApplication.class, args);
